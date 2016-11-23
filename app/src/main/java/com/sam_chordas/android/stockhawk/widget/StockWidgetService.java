d3c965ac0f8f5fb54788a10f72a04ebc70d69d03
kevin <kevin_qianjiawen@hotmail.com>
2016-11-23 15:45:22 -0500

    finish widget

--- app/src/main/java/com/sam_chordas/android/stockhawk/widget/StockWidgetService.java
+++ app/src/main/java/com/sam_chordas/android/stockhawk/widget/StockWidgetService.java
@@ -1,7 +1,15 @@
 package com.sam_chordas.android.stockhawk.widget;
 
+import android.content.Intent;
+import android.widget.RemoteViewsService;
+
 /**
  * Created by kevin on 11/23/2016.
  */
-public class StockWidgetService {
+public class StockWidgetService extends RemoteViewsService {
+
+    @Override
+    public RemoteViewsService.RemoteViewsFactory onGetViewFactory(Intent intent) {
+        return new StockWidgetFactory(getApplicationContext(), intent);
+    }
 }
